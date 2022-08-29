423aec22342d0eced4519915e5cfe993fc2cd976
KarlSavinskiy <karlsavinskiy@gmail.com>
2022-08-29 11:47:20 -0400

    commit tree 9d6f5c4ee4062ec239339d58e248dc8f5b443e18

---
+++ app.rb
@@ -0,0 +1,26 @@
+##########~~~~~~~~~~Lesson #1~~~~~~~~~~~~~~
+
+# app.rb
+# puts "Hello world!"    
+# or ypu can write puts("Hello world!") () - are optional
+# puts = console.log()   ; # = // form JS
+# puts "Hello world!" 
+ puts "Hello World!"  
+#  print "Hello world!"  
+#  print "Hello world!"  
+#  puts "Hello world!"  
+ 
+
+# puts [1,2,3]    calling  .to_s
+
+
+# p [1,2,3] calling .inspect
+
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+#  for "pretty-print" these objects by calling the .pretty_inspect
+
+#  IRV ; exit or ctrl + d 
+
+print "Pass this test, please." 
+
+p [1,2,3]
